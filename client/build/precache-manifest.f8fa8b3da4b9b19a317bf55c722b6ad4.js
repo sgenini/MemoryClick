a6706dadaf47220d115eccc11c8d47c9ef829c90
Stephane Genini <sgenini@gmail.com>
2019-01-23 21:03:08 -0800

    Deploy to Heroku

---
+++ client/build/precache-manifest.f8fa8b3da4b9b19a317bf55c722b6ad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1fa4e1c0f2a1436e03ca",
+    "url": "/static/js/main.1fa4e1c0.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "1fa4e1c0f2a1436e03ca",
+    "url": "/static/css/main.8ec9cec9.chunk.css"
+  },
+  {
+    "revision": "81377738407a6ba3b9e47c00581aa1f8",
+    "url": "/index.html"
+  }
+];
