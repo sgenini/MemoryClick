0339ced830383bcfcbbf9aa1ddebe66efa3fec7a
Stephane Genini <sgenini@gmail.com>
2019-04-03 16:08:44 -0700

    Deploy to gh-pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3aa128d2472e75144c11ee50768df7b.js"
+  "/MemoryClick/precache-manifest.ce8856d70bf70b36fa772ec9aaffa9dd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/MemoryClick/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
