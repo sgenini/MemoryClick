19363d8d46501e3a84d61d7ca4724ffe7d6aebc5
Stephane Genini <sgenini@gmail.com>
2019-04-03 16:08:28 -0700

    Updates

---
+++ precache-manifest.ce8856d70bf70b36fa772ec9aaffa9dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f48db7bbfb2d2cad17a9",
+    "url": "/MemoryClick/static/js/runtime~main.dbae6c51.js"
+  },
+  {
+    "revision": "863344379206e11fa855",
+    "url": "/MemoryClick/static/js/main.a73b7b02.chunk.js"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/MemoryClick/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "863344379206e11fa855",
+    "url": "/MemoryClick/static/css/main.63a4ad85.chunk.css"
+  },
+  {
+    "revision": "dc50633b864e5798ceded69d9255a1dc",
+    "url": "/MemoryClick/index.html"
+  }
+];
